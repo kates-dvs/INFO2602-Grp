bd2000526bda94285728af28b69b9f03272b5247
Kit <97921932+kates-dvs@users.noreply.github.com>
2023-04-21 01:13:18 -0400

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,80 @@
+import click, pytest, sys
+import csv
+
+from flask import Flask
+from flask.cli import with_appcontext, AppGroup
+
+from App.database import db, get_migrate
+from App.main import create_app
+from App.controllers import ( create_user, get_all_users_json, get_all_users )
+
+# This commands file allow you to create convenient CLI commands for testing controllers
+
+app = create_app()
+migrate = get_migrate(app)
+
+# This command creates and initializes the database
+@app.cli.command("init", help="Creates and initializes the database")
+def initialize():
+    db.drop_all()
+    db.create_all()
+    create_user('bob', 'bobpass')
+    db.session.add(bob)
+    db.session.commit()
+
+    with open('competitions.csv', newline='', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            competition = Competition(name=row['name'], category=row['category'], winner=row['winner'], runnerup=row['runnerup'])
+            dbsession.add(competition)
+        db.session.commit()
+    print('database intialized')
+
+'''
+User Commands
+'''
+
+# Commands can be organized using groups
+
+# create a group, it would be the first argument of the comand
+# eg : flask user <command>
+user_cli = AppGroup('user', help='User object commands') 
+
+# Then define the command and any parameters and annotate it with the group (@)
+@user_cli.command("create", help="Creates a user")
+@click.argument("username", default="rob")
+@click.argument("password", default="robpass")
+def create_user_command(username, password):
+    create_user(username, password)
+    print(f'{username} created!')
+
+# this command will be : flask user create bob bobpass
+
+@user_cli.command("list", help="Lists users in the database")
+@click.argument("format", default="string")
+def list_user_command(format):
+    if format == 'string':
+        print(get_all_users())
+    else:
+        print(get_all_users_json())
+
+app.cli.add_command(user_cli) # add the group to the cli
+
+'''
+Test Commands
+'''
+
+test = AppGroup('test', help='Testing commands') 
+
+@test.command("user", help="Run User tests")
+@click.argument("type", default="all")
+def user_tests_command(type):
+    if type == "unit":
+        sys.exit(pytest.main(["-k", "UserUnitTests"]))
+    elif type == "int":
+        sys.exit(pytest.main(["-k", "UserIntegrationTests"]))
+    else:
+        sys.exit(pytest.main(["-k", "App"]))
+    
+
+app.cli.add_command(test)
