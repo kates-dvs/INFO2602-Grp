fb4f9ee0c5d5a94532067ceef6d60e9d9ccd64b3
Kit <97921932+kates-dvs@users.noreply.github.com>
2023-04-24 11:28:50 -0400

    Update database.py

--- App/database.py
+++ App/database.py
@@ -6,9 +6,8 @@ db = SQLAlchemy()
 def get_migrate(app):
     return Migrate(app, db)
 
-def create_db(app):
-    db.init_app(app)
-    db.create_all(app=app)
+def create_db():
+    db.create_all()
     
 def init_db(app):
     db.init_app(app)
