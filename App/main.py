807e5fd018c4bca28cd94df691191edefd9df26b
BLY117 <97921627+BLY117@users.noreply.github.com>
2023-04-24 13:40:58 -0400

    Render Works (PLEASE)

--- App/main.py
+++ App/main.py
@@ -6,8 +6,9 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from datetime import timedelta
-from .models import User, db
+from .models import User, Competition, CompetitionUser, db
 from App.database import db
+from App.controllers import ( create_user, get_all_users_json, get_all_users )
 
 
 from App.database import init_db
